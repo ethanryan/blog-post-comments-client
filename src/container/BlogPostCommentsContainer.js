16613bbeaf3a59149c2fb597642fdf622c6f9040
ethanryan <ethanryan@gmail.com>
2018-08-27 13:03:58 -0400

    reindenting BlogPostCommentsContainer

--- src/container/BlogPostCommentsContainer.js
+++ src/container/BlogPostCommentsContainer.js
@@ -48,100 +48,102 @@ class BlogPostCommentsContainer extends Component {
 
   handleSubmitPost(username, content) {
     api.createPost(username, content)
-    .then( post => this.setState(
-      prevState => ({
-        posts: [...prevState.posts, post]
-      })
+    .then(
+      post => this.setState(
+        prevState => ({
+          posts: [...prevState.posts, post]
+        })
+      )
     )
-  )
-}
+  }
 
-handleSubmitComment(username, content) {
-  api.createComment(username, content)
-  .then( comment => this.setState(
-    prevState => ({
-      comments: [...prevState.comments, comment]
-    })
-  )
-)
-}
+  handleSubmitComment(username, content) {
+    api.createComment(username, content)
+    .then(
+      comment => this.setState(
+        prevState => ({
+          comments: [...prevState.comments, comment]
+        })
+      )
+    )
+  }
 
-handleUpdateComment(updatedComment) {
-  api.updateComment(updatedComment)
-  .then( comment =>
-    this.updateComments(comment) //calling function below
-  )
-}
+  handleUpdateComment(updatedComment) {
+    api.updateComment(updatedComment)
+    .then( comment =>
+      this.updateComments(comment) //calling function below
+    )
+  }
 
-updateComments(comment) {
-  var comments = this.state.comments.filter((eachComment) => { return eachComment.id !== comment.id })
-  //filtering all comments, excluding unedited comment...
-  comments.push(comment) //pushing edited comment into all comments...
-  this.sortCommentsById(comments) //sort comments by Id...
-  this.setState({
-    comments: comments,
-  })
-}
-
-handleDeleteComment(id) {
-  if (window.confirm("Are you sure you want to delete this comment? 😱😱😱 ")) {
-    api.deleteComment(id)
-    .then( () => {
-      this.setState( prevState => ({
-        comments: prevState.comments.filter( comment => comment.id !== id )
-      }) )
+  updateComments(comment) {
+    var comments = this.state.comments.filter((eachComment) => { return eachComment.id !== comment.id })
+    //filtering all comments, excluding unedited comment...
+    comments.push(comment) //pushing edited comment into all comments...
+    this.sortCommentsById(comments) //sort comments by Id...
+    this.setState({
+      comments: comments,
     })
   }
-}
+
+  handleDeleteComment(id) {
+    if (window.confirm("Are you sure you want to delete this comment? 😱😱😱 ")) {
+      api.deleteComment(id)
+      .then( () => {
+        this.setState( prevState => ({
+          comments: prevState.comments.filter( comment => comment.id !== id )
+        }) )
+      })
+    }
+  }
 
 
-sortCommentsById(commentsArray) {
-  commentsArray.sort(function(first, second) {
-    return first.id - second.id; //sort comments by updated_at, first to last
-  });
-  return commentsArray
-}
+  sortCommentsById(commentsArray) {
+    commentsArray.sort(function(first, second) {
+      return first.id - second.id; //sort comments by updated_at, first to last
+    });
+    return commentsArray
+  }
 
 
-handleToggleEditForm(comment) {
-  var comments = this.state.comments.filter((eachComment) => { return eachComment.id !== comment.id })
-  //filtering all comments, excluding toggled comment...
-  var editableComment = Object.assign({}, comment, {editable: true}); //make toggled comment editable...
-  comments.push(editableComment) //pushing editableComment comment into all comments...
-  this.sortCommentsById(comments) //sort comments by Id...
-  this.setState({
-    comments: comments,
-  })
-};
+  handleToggleEditForm(comment) {
+    var comments = this.state.comments.filter((eachComment) => { return eachComment.id !== comment.id })
+    //filtering all comments, excluding toggled comment...
+    var editableComment = Object.assign({}, comment, {editable: true}); //make toggled comment editable...
+    comments.push(editableComment) //pushing editableComment comment into all comments...
+    this.sortCommentsById(comments) //sort comments by Id...
+    this.setState({
+      comments: comments,
+    })
+  };
 
 
-render() {
+  render() {
 
-  console.log('state from BlogPostCommentsContainer: ', this.state)
-  return(
-    <div>
+    console.log('state from BlogPostCommentsContainer: ', this.state)
+    return(
+      <div>
 
-      <CreatePostForm
-        //props for CreatePostForm
-        handleSubmit={this.handleSubmitPost}
-      />
+        <CreatePostForm
+          //props for CreatePostForm
+          handleSubmit={this.handleSubmitPost}
+        />
 
-      <AllPosts
-        //props for AllPosts
-        posts={this.state.posts}
-        comments={this.state.comments}
-        handleUpdateComment={this.handleUpdateComment}
-        handleDeleteComment={this.handleDeleteComment}
-        handleToggleEditForm={this.handleToggleEditForm}
-        showEditForm={this.state.showEditForm}
+        <AllPosts
+          //props for AllPosts
+          posts={this.state.posts}
+          comments={this.state.comments}
+          handleUpdateComment={this.handleUpdateComment}
+          handleDeleteComment={this.handleDeleteComment}
+          handleToggleEditForm={this.handleToggleEditForm}
+          showEditForm={this.state.showEditForm}
 
-        //props for CreateCommentForm
-        handleSubmit={this.handleSubmitComment}
-      />
+          //props for CreateCommentForm
+          handleSubmit={this.handleSubmitComment}
+        />
 
-    </div>
-  )
-}
+      </div>
+    )
+  }
 }
 
 export default BlogPostCommentsContainer
