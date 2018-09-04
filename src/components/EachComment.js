498aaddd51e44d539d56e1b05af93b62d1206b2f
ethanryan <ethanryan@gmail.com>
2018-09-04 18:11:02 -0400

    cleaning up code in EachComment

--- src/components/EachComment.js
+++ src/components/EachComment.js
@@ -10,76 +10,35 @@ const EachComment = (props) => {
 
   const eachComment = allOfTheComments.filter(comment => comment.post_id === postObject.id).map( (commentObject) =>
 
-  <div key={commentObject.id ? commentObject.id : "commentObject.id here"} className="EachComment">
+  <div
+    key={commentObject.id ? commentObject.id : "commentObject.id here"}
+    className="EachComment"
+    >
 
-    {commentObject.editable === true ?
-      <EditCommentForm
-        handleSubmit={props.handleUpdateComment}
-        postId={postObject.id}
-        username={commentObject.username}
-        content={commentObject.content}
-        id={commentObject.id}
-      />
-      :
-      <EachCommentContent
-        commentObject={commentObject}
-        handleToggleEditForm={props.handleToggleEditForm}
-        handleDeleteComment={props.handleDeleteComment}
-      />
-
-      // <div>
-      // <div className="EachCommentContent">
-      //   <p>{commentObject.username ? commentObject.username : "commentObject.name here"}</p>
-      //   <p className="biggerText">{commentObject.content ? Parser(commentObject.content) : "commentObject.content here"}</p>
-      //   <p>Comment originally posted on: {commentObject.created_at ? new Date(commentObject.created_at).toLocaleString('en-US', {
-      //     weekday: 'short',
-      //     month: 'short',
-      //     day: 'numeric',
-      //     year: 'numeric',
-      //     hour: 'numeric',
-      //     minute: 'numeric',
-      //     hour12: true,
-      //     timeZoneName: 'short'
-      //   }) : "commentObject.created_at here"}</p>
-      //   <p>Updated by {commentObject.username ? commentObject.username : "commentObject.name here"} on: {commentObject.updated_at ? new Date(commentObject.updated_at).toLocaleString('en-US', {
-      //     weekday: 'short',
-      //     month: 'short',
-      //     day: 'numeric',
-      //     year: 'numeric',
-      //     hour: 'numeric',
-      //     minute: 'numeric',
-      //     hour12: true,
-      //     timeZoneName: 'short'
-      //   }) : "commentObject.updated_at here"}</p>
-      // </div>
-      //
-      //   <Card.Content extra>
-      //     <div className='ui two buttons'>
-      //
-      //       <Button
-      //         basic
-      //         color='green'
-      //         onClick={() => props.handleToggleEditForm(commentObject)}
-      //         >Edit</Button>
-      //
-      //       <Button
-      //         basic
-      //         color='red'
-      //         onClick={() => {props.handleDeleteComment(commentObject.id)}}
-      //         >Delete</Button>
-      //       </div>
-      //
-      //   </Card.Content>
-      // </div>
-    }
-  </div>
+      {
+        commentObject.editable === true ?
+        <EditCommentForm
+          handleSubmit={props.handleUpdateComment}
+          postId={postObject.id}
+          username={commentObject.username}
+          content={commentObject.content}
+          id={commentObject.id}
+        />
+        :
+        <EachCommentContent
+          commentObject={commentObject}
+          handleToggleEditForm={props.handleToggleEditForm}
+          handleDeleteComment={props.handleDeleteComment}
+        />
+      }
+    </div>
   )
 
-    return (
-      <div>
-        {eachComment}
-      </div>
-    )
-  }
+  return (
+    <div>
+      {eachComment}
+    </div>
+  )
+}
 
-  export default EachComment
+export default EachComment
